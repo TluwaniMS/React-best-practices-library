30494ee1855455215670dccda3359fe918534dc6
tluwaniMS <tluwani2020@gmail.com>
2023-11-15 05:42:39 +0200

    final-touches-to-view-rendering

--- conditional-view-rendering/ViewRenderer.correct.tsx
+++ conditional-view-rendering/ViewRenderer.correct.tsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import { SuperUserView, AdminView, UserView } from './Views';
 
+const views = {
+  SuperUser: SuperUserView,
+  Admin: AdminView,
+  User: UserView,
+};
+
+interface UserRole {
+  role: 'Admin' | 'User' | 'SuperUser';
+}
+
+export const RoleViewRenderer = (user: UserRole) => {
+  const UserView = views[user.role];
+  
+  return (
+    <div>
+      <UserView />
+    </div>
+  );
+};
